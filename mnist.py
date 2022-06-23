cc2682bcba983c02d584d55fae9bd748adc5222a
BlackLutos <gcobs172250@gmail.com>
2022-06-23 22:24:32 +0800

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -2,10 +2,8 @@
 import tensorflow as tf
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = '0'   #指定第一塊GPU可用
-config = tf.compat.v1.ConfigProto
-config.compat.v1.gpu_options.per_process_gpu_memory_fraction = 0.5  # 程序最多隻能佔用指定gpu50%的顯存
-config.compat.v1.gpu_options.allow_growth = True      #程序按需申請內存
-sess = tf.Session(config = config)
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.17)
+sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) 
 data = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = data.load_data()
 x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
